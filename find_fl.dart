f8795aa6bb3acb68b708c8a63f30e16b799616a8
Tauqeer Ahmed <tauqeer1040@gmail.com>
2026-08-04 02:52:12 +0530

    chore: remove smoke-test user data, stale tunnel dns record, and dev junk files

--- find_fl.dart
+++
@@ -1,6 +0,0 @@
-import 'dart:io';
-import 'package:shared_preferences/shared_preferences.dart';
-void main() async {
-  final prefs = await SharedPreferences.getInstance();
-  print('prefs runtime type: ${prefs.runtimeType}');
-}
--- test_radar.dart
+++
@@ -1,6 +0,0 @@
-import 'package:fl_chart/fl_chart.dart';
-
-void main() {
-  final d = RadarChartData(dataSets: [], getTitle: (i, angle) => RadarChartTitle(text: 'hi'));
-  print(d);
-}
